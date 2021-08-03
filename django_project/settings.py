42178d88ea2a666acaf8f90af4702ca9fd56948a
aadill365 <mail2adil365@gmail.com>
2021-08-03 22:08:43 +0530

    updated static

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_FILES = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
